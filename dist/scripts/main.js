fb69a1b8251da1938f02771ae48ef5664a7423f0
Thijs Van der Schaeghe <thijs@catlab.be>
2015-08-18 00:29:24 +0200

    Rebuild

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,3 +1,2 @@
 require.config({paths:{sprintf:"../vendor/sprintf/dist/sprintf.min",jquery:"../vendor/jquery/dist/jquery.min","js-cookie":"../vendor/js-cookie/src/js.cookie"},shim:{},packages:[]}),define(["i18n-lite"],function(i){return i});
-//# sourceMappingURL=main.js
 //# sourceMappingURL=main.js.map
